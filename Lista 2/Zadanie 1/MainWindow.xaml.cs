67e375fbcc8833fca743422175a183778336b068
Karolina Dbaj <k.dbaj@vp.pl>
2022-10-18 22:29:43 +0200

    zaimplementowanie prostych obliczeń

--- Lista 2/Zadanie 1/MainWindow.xaml.cs
+++ Lista 2/Zadanie 1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,16 +24,121 @@ namespace Zadanie_1
         public MainWindow()
         {
             InitializeComponent();
-            
+
+            ResultLabel.Text = string.Empty;
+            CurrentOperation.Text =string.Empty;
         }
 
         private void NumberEvent_Button(object sender, RoutedEventArgs e)
         {
-            var content = (sender as Button).Content;
+            ResultLabel.Text = string.Empty;
 
-            ResultLabel.Content += content as string;
+            var Button = sender as Button;
+
+            var currentNumber = Button.Name[Button.Name.Length-1];
+
+            CurrentOperation.Text += currentNumber;
+        }
+
+        private void buttonEqual_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperation.Text;
+
+           ResultLabel.Text = CalculateResult(operation).ToString();
+
+            CurrentOperation.Text = string.Empty;
         }
 
         
+
+        private void buttonPlus_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperation.Text;
+            if (ContainsOperation(operation))
+            {
+                CurrentOperation.Text = CalculateResult(operation).ToString();
+            }
+
+            CurrentOperation.Text += "+";
+
+        }
+
+        private void buttonMinus_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperation.Text;
+            if (ContainsOperation(operation))
+            {
+                CurrentOperation.Text = CalculateResult(operation).ToString();
+            }
+
+            CurrentOperation.Text += "-";
+
+        }
+
+        private void buttonMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperation.Text;
+            if (ContainsOperation(operation))
+            {
+                CurrentOperation.Text = CalculateResult(operation).ToString();
+            }
+            CurrentOperation.Text += "*";
+
+        }
+
+        private void buttonDivide_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperation.Text;
+            if (ContainsOperation(operation))
+            {
+                CurrentOperation.Text = CalculateResult(operation).ToString();
+            }
+            CurrentOperation.Text += "/";
+
+        }
+
+        private void buttonC_Click(object sender, RoutedEventArgs e)
+        {
+            var output = "";
+            ResultLabel.Text = output;
+        }
+
+        private bool ContainsOperation(string operation)=> operation.Contains('*') || operation.Contains('/') || operation.Contains('+') || operation.Contains('-');
+
+        
+
+        private int CalculateResult(string operation)
+        {
+            if (operation.Contains('+'))
+            {
+                var elements = operation.Split('+');
+
+               return int.Parse(elements[0]) + int.Parse(elements[1]);
+            }
+
+            if (operation.Contains('-'))
+            {
+                var elements = operation.Split('-');
+
+                return int.Parse(elements[0]) - int.Parse(elements[1]);
+            }
+
+            if (operation.Contains('/'))
+            {
+                var elements = operation.Split('/');
+
+                return int.Parse(elements[0]) / int.Parse(elements[1]);
+                
+            }
+
+            if (operation.Contains('*'))
+            {
+                var elements = operation.Split('*');
+
+                return int.Parse(elements[0]) * int.Parse(elements[1]);
+            }
+
+            return default;
+        }
     }
 }
