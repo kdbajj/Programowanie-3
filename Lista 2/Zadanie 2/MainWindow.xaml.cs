0c46c137552a8172c4fd114b09e39dfde017e214
Karolina Dbaj <k.dbaj@vp.pl>
2022-10-21 12:04:17 +0200

    rozpoczęcie zadania 2

--- Lista 2/Zadanie 2/MainWindow.xaml.cs
+++ Lista 2/Zadanie 2/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Zadanie_2
 {
@@ -20,9 +11,67 @@ namespace Zadanie_2
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
         public MainWindow()
         {
             InitializeComponent();
+
+            TicTacToe();
+        }
+        #endregion
+
+
+        #region Private Members
+
+        /// <summary>
+        /// Holds current results of cells in active game
+        /// </summary>
+        private MarkType[] mResults;
+
+        /// <summary>
+        /// True if it is player 1's turn (o) or player 2 (x)
+        /// </summary>
+        private bool mPlayer1Turn;
+
+        /// <summary>
+        ///  True if game has ended
+        /// </summary>
+        private bool mGameEnded;
+
+        /// <summary>
+        /// Starts a new game and clears all values back to the starts
+        /// </summary>
+        private void TicTacToe()
+        {
+            //Create new blank array of free cells
+            mResults = new MarkType[9];
+
+            for(var i = 0; i < mResults.Length; i++)
+            {
+                mResults[i] =MarkType.Free;
+            }
+            // Player 1 starts the game
+            mPlayer1Turn = true;
+
+           // Interate every button on the grid
+           Container.Children.Cast<Button>().ToList().ForEach(button =>
+           {
+               //Change background and foreground to default values
+               button.Content = string.Empty;
+               SolidColorBrush myBrush = new SolidColorBrush(color: Color.FromRgb(239, 206, 250));
+               button.Background = myBrush;
+               button.Foreground = myBrush;
+               
+           });
+            
+            mGameEnded = false;
+
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
+#endregion
---
+++ Lista 2/Zadanie 2/MarkType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2
+{
+    public enum MarkType
+    {
+        Free,
+        /// <summary>
+        /// Cell hasn't been clicked
+        /// </summary>
+
+        /// <summary>
+        /// Cell is a O
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// Cell is an X
+        /// </summary>
+        Cross
+    }
+}
