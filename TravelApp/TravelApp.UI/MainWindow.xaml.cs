f5cc9cefaeb92ca2a0cff9fe29b1078b18bffb64
Karolina Dbaj <k.dbaj@vp.pl>
2023-01-11 21:58:11 +0100

    created project

---
+++ TravelApp/TravelApp.UI/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TravelApp.UI.ViewModel;
+
+namespace TravelApp.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainViewModel _mainViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _mainViewModel = new MainViewModel();
+            DataContext = _mainViewModel;
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _mainViewModel.OnLoad();
+        }
+    }
+}
---
+++ TravelApp/TravelApp.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelApp.UI.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+
+        }
+
+
+
+        public void OnLoad()
+        {
+
+        }
+    }
+}
