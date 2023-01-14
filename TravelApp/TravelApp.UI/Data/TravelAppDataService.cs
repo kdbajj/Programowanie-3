2afaaed65cecdc1b8c4fcd2ad0f3d0b728098979
Karolina Dbaj <k.dbaj@vp.pl>
2023-01-14 23:48:54 +0100

    connected to database

---
+++ TravelApp/TravelApp.UI/Data/TravelAppDataService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TravelApp.DataAccess;
+using TravelApp.Model;
+
+namespace TravelApp.UI.Data
+{
+    public class TravelAppDataService
+    {
+        public IEnumerable<Travel> GetAll()
+        {
+            using (var ctx = new TravelAppDbContext())
+            {
+                return ctx.Travels.AsNoTracking().ToList();
+            }
+        }
+    }
+}
--- TravelApp/TravelApp.UI/ViewModel/MainViewModel.cs
+++ TravelApp/TravelApp.UI/ViewModel/MainViewModel.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
-
-namespace TravelApp.UI.ViewModel
+﻿namespace TravelApp.UI.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
         public string _searchBox;
         public MainViewModel()
         {
-            
+
         }
 
         public string SearchBox
